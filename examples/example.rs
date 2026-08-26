d92a5ef594226557145d3a68298f5b09b514a65d
adam <elshadore@proton.me>
2026-08-26 17:46:14 +0100

    more

--- examples/example.rs
+++ examples/example.rs
@@ -1,7 +1,7 @@
 fn main() {
     let name = String::from("examples/example.sex");
     let file = std::fs::File::open(&name).unwrap();
-    if let Err(err) = sex::parse_exprlist_reader(file) {
+    if let Err(err) = sex::parse_exprlist_reader(file, Some(name)) {
         eprintln!("{err}");
     }
 }
